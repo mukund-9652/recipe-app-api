4637580f8346ed6f6c5c05fe4481488518364eb4
Mukund R S <sit19cs040@sairamtap.edu.in>
2022-12-24 23:31:55 +0530

    docker and test update

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(5,6)
+        self.assertEqual(res,11)
+
+    def test_sub_numbers(self):
+        res = calc.subtract(10,15)
+        self.assertEquals(res,5)
