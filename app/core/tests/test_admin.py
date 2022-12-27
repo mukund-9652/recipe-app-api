b6f6cde4559befe8bb5eaa1b534d0f61ca31d013
Mukund R S <sit19cs040@sairamtap.edu.in>
2022-12-27 21:27:37 +0530

    admin updates

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -10,17 +10,27 @@ class AdminSiteTest(TestCase):
         # Create user and client
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email="superUser@example.com", password="test@123!",
+            email="superUser@example.com",
+            password="test@123!",
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email="user2@example.com", password="Testing", name="Test User",
+            email="user2@example.com",
+            password="Testing",
+            name="Test User",
         )
 
     def test_users_list(self):
         # Test that users are listed
-        url = reverse("admin:code_user_changelist")
+        url = reverse("admin:core_user_changelist")
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        # Test if userpage works
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
