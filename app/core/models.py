93e64c60391b4165c796f1d9e87035c2663e8f0e
Mukund R S <sit19cs040@sairamtap.edu.in>
2022-12-27 19:44:28 +0530

    super user creation

--- app/core/models.py
+++ app/core/models.py
@@ -8,11 +8,16 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_field):
-        user = self.model(email=email, **extra_field)
+        if not email:
+            raise ValueError("User Must need email address")
+        user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+    
+    def create_superuser(self, email, password=None, **extra_field):
+        return ""
 
 
 class User(AbstractBaseUser, PermissionsMixin):
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,3 +12,28 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        sample_email = [
+            ["xyz@GMAIL.com", "xyz@gmail.com"],
+            ["xyz2@GMAIL.COM", "xyz2@gmail.com"],
+            ["Mukund_2001@Gmail.com", "Mukund_2001@gmail.com"],
+            ["MUKUND_2002@GMAIL.com", "MUKUND_2002@gmail.com"],
+        ]
+
+        for email, expected in sample_email:
+            user = get_user_model().objects.create_user(email, "sample123")
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('','123456')
+            
+    def test_create_superuser(self):
+        user=get_user_model().objects.create_superuser(
+            "test_superuser@example.com",
+            '1234567890'
+        )
+        
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
