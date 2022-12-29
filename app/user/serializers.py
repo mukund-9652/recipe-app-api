f41b17fc3d4162b184f8c30d39c59df5c739a126
Mukund R S <sit19cs040@sairamtap.edu.in>
2022-12-29 19:44:19 +0530

    api completed test builds

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -25,6 +25,15 @@ class UserSerializer(serializers.ModelSerializer):
         # Create and return user with encrypted password
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        # Update and return the updated data
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     # Serializer for user Auth Token
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -19,6 +19,7 @@ from rest_framework import (
 
 CREATE_USER_URL = reverse("user:create")
 TOKEN_URL = reverse("user:token")
+ME_URL = reverse("user:me")
 
 
 def create_user(
@@ -178,3 +179,48 @@ class PublicUserApiTests(TestCase):
             res.status_code,
             status.HTTP_400_BAD_REQUEST,
         )
+
+    def test_retrieve_user_unauthorized(self):
+        # Test authentication is required for user
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    # Test private api requests with authentications
+    def setUp(self):
+        self.user = create_user(
+            email="test_private@example.com",
+            password="PasswordPrivate!",
+            name="Private Test Name",
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_Retrieve_profile_success(self):
+        # Test retrieving profile for logged in user
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            res.data, {"name": self.user.name, "email": self.user.email}
+        )
+    def test_post_me_not_available(self):
+        # Test POST METHOD IS DESABLED OR NOT 
+        
+        res=self.client.post(ME_URL)
+        
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        
+    def test_update_user_profile(self):
+        # Test to update user profile for authenticated user 
+        payload={
+            'name': "updated name",
+            'password': 'newpassword123',
+        }    
+        res=self.client.patch(ME_URL, payload)
+        
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
